dc768180043748964f43ad94b8b321b86020cbd8
Mr-Zhou-S-H <964636102@qq.com>
2019-12-08 17:48:12 +0800

    .

---
+++ src/pages/index/index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="home" @click="clickHandle">
+    <image class="bg-img" src='/static/images/bird/birdBg.png'></image>
+    <div class="main">
+      <talk></talk>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+import talk from '@/components/talk'
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    card,
+    talk
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.home {
+  position: relative;
+}
+.bg-img {
+  position: absolute;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+}
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
