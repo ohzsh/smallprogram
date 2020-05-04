351c661402831cda14c42e2030c1aa1b202e14f0
Mr.zhou <zzmy1995@gmail.com>
2020-05-04 17:39:06 +0800

    feat:addurl

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -28,6 +28,7 @@ export default {
             imgUrls: [
                 'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
                 'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
+                'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
             ],
         }
     },
